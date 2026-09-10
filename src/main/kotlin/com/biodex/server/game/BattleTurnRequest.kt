57d384df65ec62d5f6b6a7cf723c49abeb203d81
srishtihirani-prog <srishtichirani@gmail.com>
2026-09-11 01:46:38 +0530

    Updated DTOs and JSON serialization config

--- src/main/kotlin/com/biodex/server/game/BattleTurnRequest.kt
+++ src/main/kotlin/com/biodex/server/game/BattleTurnRequest.kt
@@ -1,2 +1,10 @@
 package com.biodex.server.game
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BattleTurnRequest(
+    val battleId: String = "",
+    val moveId: String = "",
+    val turnNumber: Int = 1,
+)
--- src/main/kotlin/com/biodex/server/routes/AnswerRequest.kt
+++ src/main/kotlin/com/biodex/server/routes/AnswerRequest.kt
@@ -1,2 +1,11 @@
 package com.biodex.server.routes
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AnswerRequest(
+    val questionId: String,
+    val speciesId: String,
+    val selectedOptionIndex: Int,
+    val timeTakenSeconds: Double
+)
